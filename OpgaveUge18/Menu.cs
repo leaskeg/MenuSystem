8a8a353527156ad6d99db0fc552e010d060b2e49
Jens Bach <leaskegaming@gmail.com>
2022-05-09 10:29:19 +0200

    Add project files.

---
+++ OpgaveUge18/Menu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuSystem
+{
+    public class Menu
+    {
+        public string Title { get; set; }
+        public int ItemCount = 0;
+        public MenuItem[] MenuItems = new MenuItem[10];
+       
+        public void Show()
+        {
+            while (true)
+            {
+                Console.WriteLine(Title + "\n");
+                for (int i = 0; i < ItemCount; i++)
+                {
+
+                    Console.WriteLine("  " + MenuItems[i].Title);
+
+                }
+                Console.WriteLine("\n(Tryk menupunkt eller 0 for at afslutte)");
+                
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+        }
+        public void AddMenuItem()
+        {
+            MenuItems[0] = new MenuItem { };
+        }
+        public Menu()
+        {
+                
+        }
+    }
+}
---
+++ OpgaveUge18/MenuItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MenuSystem
+{
+    public class MenuItem
+    {
+        public string Title;
+        public MenuItem()
+        {
+            string itemTitle;
+        }
+    }
+
+}
